e00013a0679305cd310f691f105f1fef14bf343c
zhyu1207 <zhy47@126.com>
2017-11-19 14:49:55 +0800

    urgent update: dataInitialiseBean

--- CrazyAuction/CrazyAuction-ejb/src/java/ejb/session/singleton/DataInitialisationBean.java
+++ CrazyAuction/CrazyAuction-ejb/src/java/ejb/session/singleton/DataInitialisationBean.java
@@ -5,7 +5,9 @@
  */
 package ejb.session.singleton;
 
+import entity.Customer;
 import entity.Employee;
+import java.math.BigDecimal;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.LocalBean;
@@ -13,6 +15,8 @@ import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import static util.enumeration.AccessRightEnum.ADMIN;
+import static util.enumeration.AccessRightEnum.FINANCE;
+import static util.enumeration.AccessRightEnum.SALES;
 
 /**
  *
@@ -32,22 +36,35 @@ public class DataInitialisationBean {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-
     public DataInitialisationBean() {
     }
+
     @PostConstruct
-    public void postConstruct()
-    {
-        if(em.find(Employee.class, 1l) == null)
-        {
-            initialiseData();
+    public void postConstruct() {
+        if (em.find(Employee.class, 1l) == null) {
+            initialiseEmployeeData();
+        }
+        if (em.find(Customer.class, 1l) == null) {
+            initialiseCustomereeData();
         }
     }
-    
-    private void initialiseData()
-    {
+
+    private void initialiseEmployeeData() {
         Employee employee = new Employee("Bob", "Brown", "BobManager", "password", ADMIN);
         em.persist(employee);
-        
+        employee = new Employee("Alice", "White", "AliceFin", "password", FINANCE);
+        em.persist(employee);
+        employee = new Employee("Charles", "Woods", "CharlesSale", "password", SALES);
+        em.persist(employee);
+
+    }
+
+    private void initialiseCustomereeData() {
+        Customer customer = new Customer("Xiao", "Ming", "G00000001", "password", new BigDecimal(0), "xiaoming@test.com");
+        em.persist(customer);
+        customer = new Customer("Xiao", "Hong", "G00000002", "password", new BigDecimal(0), "xiaohong@test.com");
+        em.persist(customer);
+        customer = new Customer("Xiao", "Hua", "G00000003", "password", new BigDecimal(0), "xiaohua@test.com");
+        em.persist(customer);
     }
 }
